02b94c215a49a4528ab8c4397198f1e0a09cbd17
Nikita <nik.volodchenkov@gmail.com>
2025-05-02 11:06:32 +0300

    задание с волокнами сделано

--- fibers.cpp
+++ fibers.cpp
@@ -9,6 +9,7 @@ typedef struct {
 } DATA, * PDATA;
 
 static LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+void WINAPI MyFiberFunction(LPVOID);
 static HWND hwnd;
 WCHAR szFiberChildClass[100] = L"fiberchild";
 static int width = 600, height = 300;
@@ -16,20 +17,45 @@ HDC hdc;
 
 static PDATA pDataArray[2];
 LPVOID FiberArray[2], mainFiber;
-BOOL stopfibers = false, end = false;
 
 DWORD WINAPI FiberMessageCycle(LPVOID) {
+    for (int i = 0; i < 2; i++) {
+        pDataArray[i] = (PDATA)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(DATA));
+        if (pDataArray[i] == NULL) return -1;
+        pDataArray[i]->numb = i;
+    }
+    pDataArray[0]->rect = { 0, 0, width / 2, height };
+    pDataArray[1]->rect = { width / 2, 0, width, height };
+
+    mainFiber = ConvertThreadToFiber(nullptr);
+    for (int i = 0; i < 2; i++) FiberArray[i] = CreateFiber(0, MyFiberFunction, pDataArray[i]);
+
     HINSTANCE hInstance = GetModuleHandle(nullptr);
     RegisterFiberChild(hInstance);
     hwnd = CreateWindow(szFiberChildClass, L"Здесь работают волокна", WS_CAPTION, CW_USEDEFAULT, 0, width, height, NULL, NULL, hInstance, NULL);
     ShowWindow(hwnd, SW_SHOW);
     UpdateWindow(hwnd);
+    hdc = GetDC(hwnd);
+
+    bool running = true;
+    DWORD time = GetTickCount();
 
     MSG msg;
-    while (GetMessage(&msg, nullptr, 0, 0)) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
+    while (running) {
+        while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+            if (msg.message == WM_QUIT) {
+                running = false;
+                break;
+            }
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        if (running && GetTickCount() - time >= 100) {
+            time = GetTickCount();
+            SwitchToFiber(FiberArray[0]);
+        }
     }
+    ConvertFiberToThread();
     return 0;
 }
 
@@ -58,35 +84,22 @@ HWND GetFiberChild() {
     return hwnd;
 }
 
-DWORD WINAPI MyFiberFunction(LPVOID lpParam) {
+void WINAPI MyFiberFunction(LPVOID lpParam) {
     PDATA data = (PDATA)lpParam;
     HBRUSH brush;
     srand((int)data->numb);
-    while (not stopfibers) {
+    while (true) {
         brush = CreateSolidBrush(RGB(rand() % 256, rand() % 256, rand() % 256));
         FillRect(hdc, &(data->rect), brush);
         DeleteObject(brush);
-        Sleep(100);
+        if (data->numb == 0) SwitchToFiber(FiberArray[1]);
+        if (data->numb == 1) SwitchToFiber(mainFiber);
     }
-    return 0;
 }
 
 static LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
     switch (message)
     {
-    case WM_CREATE:
-    {
-        ShowWindow(hWnd, SW_SHOW);
-        //for (int i = 0; i < 2; i++) {
-        //    pDataArray[i] = (PDATA)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(DATA));
-        //    if (pDataArray[i] == NULL) DestroyWindow(hWnd);
-        //    pDataArray[i]->numb = i;
-        //}
-        //LPVOID mainFiber = ConvertThreadToFiber(nullptr);
-        //pDataArray[0]->rect = { 0, 0, width / 2, height };
-        //pDataArray[1]->rect = { width / 2, 0, width, height };
-    }
-    break;
     case WM_PAINT:
     {
         PAINTSTRUCT ps;
@@ -96,13 +109,12 @@ static LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM l
     break;
     case WM_DESTROY:
     {
-        //for (int i = 0; i < 2; i++) {
-        //    DeleteFiber(FiberArray[i]);
-        //    if (pDataArray[i] != NULL)
-        //        HeapFree(GetProcessHeap(), 0, pDataArray[i]);
-        //    ConvertFiberToThread;
-        //}
-        //stopfibers = true;
+        for (int i = 0; i < 2; i++) {
+            DeleteFiber(FiberArray[i]);
+            if (pDataArray[i] != NULL)
+                HeapFree(GetProcessHeap(), 0, pDataArray[i]);
+        }
+        ReleaseDC(hwnd, hdc);
         PostQuitMessage(0);
         return DefWindowProc(hWnd, message, wParam, lParam);
     }
